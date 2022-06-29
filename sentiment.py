cea60096136eeb339e8e94c5a60e1311191fe7fe
osidekyle <hawkinskyle67@gmail.com>
2022-06-29 00:53:14 -0700

    feeding model reviews for testing

--- sentiment.py
+++ sentiment.py
@@ -2,26 +2,26 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-# data = keras.datasets.imdb
-#
-# (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
-#
-# print(train_data[0])
-#
-# word_index = data.get_word_index()
-#
-# word_index = {k:(v+3) for k,v in word_index.items()}
-# word_index['<PAD>'] = 0
-# word_index['<START>'] = 1
-# word_index['<UNK>'] = 2
-# word_index['<UNUSED>'] = 3
-#
-# reverse_word_index = dict([(value, key) for (key,value) in word_index.items ()])
-#
-#
-# train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], maxlen=250)
-# test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], maxlen=250)
-#
+data = keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=10000)
+
+print(train_data[0])
+
+word_index = data.get_word_index()
+
+word_index = {k:(v+3) for k,v in word_index.items()}
+word_index['<PAD>'] = 0
+word_index['<START>'] = 1
+word_index['<UNK>'] = 2
+word_index['<UNUSED>'] = 3
+
+reverse_word_index = dict([(value, key) for (key,value) in word_index.items ()])
+
+
+train_data = keras.preprocessing.sequence.pad_sequences(train_data, value=word_index["<PAD>"], maxlen=250)
+test_data = keras.preprocessing.sequence.pad_sequences(test_data, value=word_index["<PAD>"], maxlen=250)
+
 # def decode_review(text):
 #     return " ".join([reverse_word_index.get(i, "?") for i in text])
 #
@@ -49,5 +49,25 @@ import numpy as np
 #
 # model.save("model.h5")
 
+def review_encode(s):
+    encoded = [1]
+
+    for word in s:
+        if word.lower() in word_index:
+            encoded.append(word_index[word.lower()])
+        else:
+            encoded.append(2)
+    return encoded
+
 model = keras.models.load_model("model.h5")
+
+with open("test.txt", encoding="utf-8") as f:
+    for line in f.readlines():
+        nline = line.replace(",","").replace(".","").replace("{","").replace("}","").replace(":","").replace("\"","").strip().split(" ")
+        encode = review_encode(nline)
+        encode = keras.preprocessing.sequence.pad_sequences([encode], value=word_index["<PAD>"], maxlen=250)
+        predict = model.predict(encode)
+        print(line)
+        print(encode)
+        print(predict[0])
 
